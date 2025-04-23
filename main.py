76997e621e90d3d127ea1599b0a468f97ba72e8b
Matt <matt.mennella@gmail.com>
2025-04-23 07:38:01 -0400

    Added pygame gui output

--- main.py
+++ main.py
@@ -3,15 +3,20 @@ from facial_recognition import FacialRecognitionHandler
 import threading
 import time
 
+pygame.init()
+
 class SmartMirror:
 
     def __init__(self):
         self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        self.screen_rect = self.screen.get_rect()
+        self.font = pygame.font.SysFont(None, size=48)
         self.facial_recogntion = FacialRecognitionHandler()
         self.in_frame = []
         self.in_frame_datalock = threading.Lock()
         self.scan_for_faces = True
-    
+        self._to_draw = []
+
     def _update_in_frame(self):
         while self.scan_for_faces:
             with self.in_frame_datalock:
@@ -30,21 +35,51 @@ class SmartMirror:
                     in_frame_copy.remove(remove_name)
             with self.in_frame_datalock:
                 self.in_frame = in_frame_copy[:]
+                print(self.in_frame)
             time.sleep(1)
 
-    def in_frame_thread_handler(self):
+    def _in_frame_thread_handler(self):
         in_frame_thread = threading.Thread(
             target=self._update_in_frame,
             daemon=True
         )
         in_frame_thread.start()
 
+    def _create_name_rect(self):
+        with self.in_frame_datalock:
+            in_frame_copy = self.in_frame[:]
+        
+        if not in_frame_copy:
+            return None
+        name_str = ''
+        if len(in_frame_copy) == 1:
+            name_str += in_frame_copy[0]
+        else:
+            for x in range(len(in_frame_copy)):
+                if x == len(in_frame_copy) - 1:
+                    name_str += in_frame_copy[x]
+                else:
+                    name_str += in_frame_copy[x] + ', '
+
+        names = self.font.render(name_str, True, (255,255,255))
+        names_rect = names.get_rect()
+        names_rect.center = self.screen_rect.center
+        self._to_draw.append((names, names_rect))
+
+    def draw_screen(self):
+        self.screen.fill((0,0,0))
+        self._create_name_rect()
+        self.screen.blits(blit_sequence=self._to_draw)
+        pygame.display.flip()
+        self._to_draw=[]
+
     def run_program(self):
+        self._in_frame_thread_handler()
         while True:
-            print(self.in_frame)
-            time.sleep(1)
+            self.draw_screen()
+
+
 
 if __name__ == '__main__':
     smart_mirror = SmartMirror()
-    smart_mirror.in_frame_thread_handler()
     smart_mirror.run_program()
